ea9689c43a1f68c8f59951063f6abd53b859725a
IcedDino <nullpointer@floresr.com>
2025-12-02 06:48:05 -0600

    all

--- app/main.py
+++ app/main.py
@@ -87,13 +87,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://esiimav3-frontend-cchnfzcbbzgegucu.mexicocentral-01.azurewebsites.net",
-        "http://localhost",
-        "http://localhost:8080",
-        "http://localhost:3000",
-        "http://127.0.0.1:5500"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
